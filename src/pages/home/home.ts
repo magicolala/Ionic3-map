fc471dc648c4bac1dfe72e5bd60a89f81f3e7e44
Cédric Oloa <cedric.oloa@laposte.net>
2018-01-04 18:44:52 +0100

    init

---
+++ src/pages/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
